95bb3148bce0e498e27ae98bcf619dac944e6c43
Marcelo Braga <matigela@gmail.com>
2021-05-01 11:10:48 -0300

    check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -22,5 +22,7 @@ public abstract class ChessPiece extends Piece{
 		return p != null && p.getColor() != color;
 	}
 	
-	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
 }
